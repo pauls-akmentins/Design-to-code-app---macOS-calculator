7368dbeb3f34b5689a707bb071e5421f830b601b
ui-pauls-akmentins <pauls.akmentins@ui.com>
2022-06-12 18:21:31 +0300

    Add dummy button alert

--- src/components/CalculatorComponents/CalculatorCirclesRow/CalculatorCirclesRow.tsx
+++ src/components/CalculatorComponents/CalculatorCirclesRow/CalculatorCirclesRow.tsx
@@ -3,27 +3,32 @@ import styles from "./CalculatorCirclesRow.module.css";
 import { CircleType } from "./types";
 import { getCircleType } from "./utls";
 
-const CalculatorCirclesRow: React.FC = () => (
-  <div className={classNames(styles.topActionRowWrapper)}>
-    {Object.values(CircleType).map((circleType, i) => (
-      <div
-        key={`${circleType}-${i}`}
-        className={classNames(
-          styles.circleButtonContainer,
-          styles[`circleButtonContainer-${circleType}`]
-        )}
-      >
-        <span
-          className={classNames(
-            styles.circleButton,
-            styles[`circleButton-${circleType}`]
-          )}
-        >
-          {getCircleType(circleType)}
-        </span>
-      </div>
-    ))}
-  </div>
-);
+const CalculatorCirclesRow: React.FC = () => {
+  const handleClick = (circleType: CircleType) =>
+    alert("Just a dummy button, what else you expected?");
 
+  return (
+    <div className={classNames(styles.topActionRowWrapper)}>
+      {Object.values(CircleType).map((circleType, i) => (
+        <div
+          key={`${circleType}-${i}`}
+          className={classNames(
+            styles.circleButtonContainer,
+            styles[`circleButtonContainer-${circleType}`]
+          )}
+          onClick={() => handleClick(circleType)}
+        >
+          <span
+            className={classNames(
+              styles.circleButton,
+              styles[`circleButton-${circleType}`]
+            )}
+          >
+            {getCircleType(circleType)}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default CalculatorCirclesRow;
