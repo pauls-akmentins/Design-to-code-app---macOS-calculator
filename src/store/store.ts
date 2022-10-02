7f95242f3d7a81c269db2823e40f578140de09a5
Pauls Akmentins <paulsakmentins@Paulss-MacBook-Pro.local>
2022-10-02 15:50:28 +0300

    Remove comments slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, Store } from "@reduxjs/toolkit";
-import { commentsSlice } from "./comments";
 import { postsSlice } from "./posts";
 
 export const store: Store<any, any> = configureStore({
   reducer: {
-    comments: commentsSlice.reducer,
-    posts: postsSlice.reducer
+    posts: postsSlice.reducer,
   },
 });
